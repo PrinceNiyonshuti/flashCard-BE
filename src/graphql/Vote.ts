29b25bed8a28811b20fac189fd9bd637e0584a1c
PrinceNiyonshuti <npprince47@gmail.com>
2022-11-01 20:48:44 +0200

    (ft-voting): Implement the resolve function for the vote mutation

--- src/graphql/Vote.ts
+++ src/graphql/Vote.ts
@@ -1,6 +1,7 @@
 import { objectType, extendType, nonNull, intArg } from "nexus";
+import { User } from "@prisma/client";
 
-export const Vote = objectType({  // 1
+export const Vote = objectType({ 
     name: "Vote",
     definition(t) {
         t.nonNull.field("link", { type: "Link" });
@@ -8,7 +9,7 @@ export const Vote = objectType({  // 1
     },
 });
 
-export const VoteMutation = extendType({  // 2
+export const VoteMutation = extendType({
     type: "Mutation",
     definition(t) {
         t.field("vote", {
@@ -16,7 +17,34 @@ export const VoteMutation = extendType({  // 2
             args: {
                 linkId: nonNull(intArg()),
             },
-            
+            async resolve(parent, args, context) {
+                const { userId } = context;
+                const { linkId } = args;
+
+                if (!userId) {  // 1 
+                    throw new Error("Cannot vote without logging in.");
+                }
+
+                const link = await context.prisma.link.update({  // 2
+                    where: {
+                        id: linkId
+                    },
+                    data: {
+                        voters: {
+                            connect: {
+                                id: userId
+                            }
+                        }
+                    }
+                })
+
+                const user = await context.prisma.user.findUnique({ where: { id: userId } });
+
+                return {  // 3
+                    link,
+                    user: user as User
+                };
+            },
         })
     }
 })
